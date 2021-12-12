250b1735883594ac3011d71d02981a2c36707052
EricHuang <e8321eric@gmail.com>
2021-12-13 07:35:04 +0800

    Add Whitenoise for heroku static files

--- simplesite/settings.py
+++ simplesite/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -85,6 +86,10 @@ TEMPLATES = [
         },
     },
 ]
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 WSGI_APPLICATION = 'simplesite.wsgi.application'
 
