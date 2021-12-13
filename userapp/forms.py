064855448ed3a96d87520b45b495ddd8a98ef8dc
EricHuang <e8321eric@gmail.com>
2021-12-13 08:36:01 +0800

    Comment out unused forms

--- userapp/forms.py
+++ userapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+""" 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -8,4 +9,5 @@ class SignUpForm(UserCreationForm):
   
   class Meta:
     model = User
-    fields = ('username', 'email', 'password1', 'password2', )
+    fields = ('username', 'email', 'password1', 'password2', ) 
+"""
