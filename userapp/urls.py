8fbfafec46434ad7d69aeb48941c159e06177aa6
EricHuang <e8321eric@gmail.com>
2021-12-13 08:35:22 +0800

    Redirect root url to 'account/login'

--- userapp/urls.py
+++ userapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-
 from django.conf.urls import include
+from django.views.generic import RedirectView
+
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
+    path('', RedirectView.as_view(url='accounts/login/')),
     path('profile/', views.profile, name='profile'),
     path('accounts/', include('allauth.urls')),
 ]
