48964c86daa731853589d2d16811bf1a437c01a4
EricHuang <e8321eric@gmail.com>
2021-12-13 09:49:42 +0800

    Sending email by google SMTP

--- simplesite/settings.py
+++ simplesite/settings.py
@@ -213,5 +213,12 @@ SOCIALACCOUNT_EMAIL_VERIFICATION = "none"
 # This will avoid the need for an SMTP server as e-mails will be printed to the console.
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# Sending email by google SMTP server.
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 25
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
